8fd50f380ed1c4fbffe559768acf9528d8932151
mmbunde <mmbunde@duck.com>
2023-09-05 19:06:18 -0600

    fixed formatting

--- BundeGreen_HW2_Encrypt.java
+++ BundeGreen_HW2_Encrypt.java
@@ -8,32 +8,32 @@ public class BundeGreen_HW2_Encrypt{
         int[] K = new int[4];
         int[] L = new int[3];
         int[] R = new int[3];
+        R[0] = 0x8000006b;
+        L[1] = R[0];
         Scanner keyboard = new Scanner(System.in);
         
         for(int i = 0; i < 4; i++){
             System.out.println("Please enter your hex string K[" + i + "] without the '0x'");
             K[i] = Integer.parseUnsignedInt(keyboard.nextLine(), 16);
         }
-        R[0] = 0x8000006b;
-        L[1] = R[0];
+
         System.out.println("Please enter your hex string for L[0] without the '0x'");
         L[0] = Integer.parseUnsignedInt(keyboard.nextLine(), 16);
+        
         int f = ((R[0] << 4) + K[0]) ^ (R[0] + deltaOne) ^ ((R[0] >>> 5) + K[1]);
-    
         R[1] = L[0] + f;
         L[2] = R[1];
         int g = ((R[1] << 4) + K[2]) ^ (R[1] + deltaTwo) ^ ((R[1] >>> 5) + K[3]);
         R[2] = L[1] + g;
         
-        System.out.println(Integer.toUnsignedString(f));
         for(int i = 0; i < 4; i++){
             System.out.println("K[" + i + "]: " + Integer.toUnsignedString(K[i]));
         }
         for(int i = 0; i < 3; i++){
             System.out.printf("R[%d] = 0x%08X%n", i, R[i]);
+        }
+        for(int i = 0; i < 3; i++){
             System.out.printf("L[%d] = 0x%08X%n", i, L[i]);
         }
-        System.out.printf("f = 0x%08X%n",f);
-        System.out.printf("g = 0x%08X%n",g);
     }
 }
